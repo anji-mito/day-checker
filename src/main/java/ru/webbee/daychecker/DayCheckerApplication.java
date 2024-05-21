ca48fe6d987f4762dc02a5ccbc27d735c932217e
anji-mito <120057673+anji-mito@users.noreply.github.com>
2024-05-21 23:22:26 +0500

    add: parsing

--- src/main/java/ru/webbee/daychecker/DayCheckerApplication.java
+++ src/main/java/ru/webbee/daychecker/DayCheckerApplication.java
@@ -4,7 +4,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.webbee.daychecker.dateсhecker.model.Day;
 
